7465f4d44761e39b9b01698a56f22d4c9538ddb8
Mihai Bizovi <bizovim@gmail.com>
2018-06-24 19:29:30 +0300

    Tiny RPSLS game

--- python_interactive_programming_pt1.py
+++ python_interactive_programming_pt1.py
@@ -1,7 +1,7 @@
 # python interactive programming coursera
 # import numpy as np
 import math
-import types
+import random
 
 '''
 There are 52805280 feet in a mile.
@@ -85,6 +85,65 @@ def triangle_area(coord_a, coord_b, coord_c):
     area = math.sqrt(s * (s - a) * (s - b) * (s - c))
     return area
 
+'''
+Write a Python function
+print_digits that takes an integer number in the range [0,100)
+It prints the message "The tens digit is %, and the ones digit is %"
+'''
+def print_digits(x):
+    if x >= 100 or x < 0:
+        print('A number in range [0, 100) needed')
+    else:
+        tens = x // 10
+        ones = x % 10
+        message = 'The tens digit is {} and the ones digit is {}'.format(str(tens), str(ones))
+        print(message)
+
+'''
+Powerball: 6 numbers drawn at random
+Players can purchase a lottery ticket with a specific number combination and,
+if the number on the ticket matches the numbers generated in a random drawing,
+the player wins a massive jackpot.
+'''
+def powerball():
+    # random integers in range [1, 60), allowing for duplicates
+    draws = []
+    while len(draws) != 6:
+        x = random.randrange(0, 60)
+        if x not in draws:
+            draws.append(x)
+
+    # powerball number in range [1, 36)
+    powerball = random.randrange(1, 36)
+    print('Today\'s numbers are {}, {}, {}, {}, {}, {}. The powerball number is {}'.format(*draws, powerball))
+
+'''
+Pig latin game:
+If first letter consonant append consonant + 'ay': 'pig' -> 'igpay'
+If first letter vowel append 'way' to the end of the word: 'owl' -> 'owlway'
+'''
+def pig_latin(word):
+    word = word.lower()
+    if word[0] not in ['a', 'e', 'i', 'o', 'u']:
+        return word[1:len(word)] + 'ay'
+    else:
+        return word + 'way'
+
+'''
+IN: coefficients of ax^2 + bx + c = 0
+OUT: solution
+This would be more neat with vectorization
+'''
+def quadratic_eq(a, b, c):
+    determinant = b ** 2 - 4*a*c
+    if determinant >= 0:
+        solution_1 = (-b + math.sqrt(determinant)) / 2*a
+        solution_2 = (-b - math.sqrt(determinant)) / 2*a
+    else:
+        solution_1 = b / (2*a) + complex(0, 1* math.sqrt(abs(determinant))) / (2*a)
+        solution_2 = b / (2*a) - complex(0, 1* math.sqrt(abs(determinant))) / (2*a)
+    return [solution_1, solution_2]
+
 if __name__ == '__main__':
     print('Number of feet in 13 miles is', feet_to_mile(feet=13), 'ft')
     print('Number of seconds is', to_seconds(seconds = 37, minutes = 21, hours=7), 'sec')
@@ -99,3 +158,12 @@ if __name__ == '__main__':
     # expected result of the below is (3 ** 2) / 2 = 4.5
     print('The triangle area is',
         round(triangle_area(coord_a=[0, 0], coord_b=[0,3], coord_c=[3,0]), 2), 'sq. in')
+    print(print_digits(57))
+    powerball()
+
+    # conditionals
+    print(pig_latin('Owl'), pig_latin('piG'))
+    # expected solution [2, 1]
+    print('The solution to q.eq. is', quadratic_eq(a=1, b=-3, c=2))
+    # expected solution to following 1/6 +/- i*sqrt(17)/6
+    print('The solution to q.eq. is', quadratic_eq(a=6, b=-2, c=3))
