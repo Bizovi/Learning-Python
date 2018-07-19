ee676c24ec5e4371722cc160df80e7dd673d1c14
Mihai Bizovi <bizovim@gmail.com>
2018-07-19 21:48:40 +0300

    async programming basics

---
+++ advanced_python/unit_testing.py
@@ -0,0 +1,26 @@
+# unit_testing.py
+# protect against human mistake changing code, method, functions
+import unittest
+
+# class Phonebook():
+#
+#     def __init__(self):
+#         selft.entries = {}
+#
+#     def add(self, name, number):
+#         self.entries[name] = number
+#
+# 
+#
+# def setUp(self):
+#     self.phonebook = Phonebook()
+#
+# def test_entry(self):
+#     pass
+#
+# def tearDown(self):
+#     pass
+#
+#
+# if __name__ == "__main__":
+#     assertEqual(5, 2)
